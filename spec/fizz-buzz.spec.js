f608150a47cac32f5bbf7e8fee0ea7ab8c67d290
Becca <beccaburns89@gmail.com>
2019-09-14 15:23:33 +0200

    Adds it block to spec file and adds implementation code to fizzbuzz file.

--- spec/fizz-buzz.spec.js
+++ spec/fizz-buzz.spec.js
@@ -6,3 +6,9 @@ describe('FizzBuzz', () => {
     it('returns a number if no game rules are met', () => {
         expect(fizzBuzz.check(1)).to.equal(1)
     })
+    
+    it('returns Fizz if number is divisible by 3', () => {
+        expect(fizzBuzz.check(3)).to.equal('Fizz')
+    })
+
+    
---
+++ src/js/fizzbuzz.js
@@ -0,0 +1,18 @@
+function FizzBuzz()  {
+    this.check = (number) => {
+        return number
+    }
+}
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = FizzBuzz;
+    } 
+
+function FizzBuzz() {
+        this.check = (number) => {
+            if (number % 3 === 0) {
+                return 'Fizz';
+            } else {
+                return number
+            }
+        }
+    }
